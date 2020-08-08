1bc7c2b97a3392272c18414ca60ff080fa747dac
Amanya-Brian <65342608+Amanya-Brian@users.noreply.github.com>
2020-08-08 11:39:28 +0300

    third commit

--- script.js
+++ script.js
@@ -1,4 +1,71 @@
 function getContent(){
     return document.getElementById("content-value").innerText;
 }
-alert(getContent());
+function returnContent(num){
+    document.getElementById("content-value").innerText = num;
+}
+function getResult(){
+    return document.getElementById("result-value").innerText;
+}
+function returnResult(num){
+    if( num == ""){
+        document.getElementById("result-value").innerText = num;
+    }
+    else{
+        document.getElementById("result-value").innerText = getFormattedValue(num);
+    }
+}
+function getFormattedValue(num){
+    var n = Number(num);
+    var value = n.toLocaleString("en");
+    return value;
+}
+function reverseFormattedValue(num){
+    return Number(num.replace(/,/g,""));
+}
+
+var operator = document.getElementsByClassName("operators");
+
+for(var i = 0; i < operator.length; i++){
+    operator[i].addEventListener('click',function(){
+        if(this.id == "clear"){
+            returnContent("");
+            returnResult("");
+        }
+        else if(this.id == "delete"){
+            var output = reverseFormattedValue(getResult()).toString();
+            if(output){
+                output = output.substr(0,output.length-1);
+                returnResult(output);
+            }
+        }
+        else{
+            var output = getResult();
+            var content = getContent();
+            if(output != ""){
+                output = reverseFormattedValue(output);
+                output +=  this.innerText;
+                content += output;
+                returnContent(content);
+                returnResult("");
+                if(this.id == "equals"){
+                    var output = eval(content);
+                    returnResult(output);
+                    returnContent("");
+                }
+            }
+        }
+    })
+}
+
+var number = document.getElementsByClassName("number");
+
+for(var j = 0; j < number.length; j++){
+    number[j].addEventListener('click',function(){
+        var output = reverseFormattedValue(getResult());
+        if(output != NaN){
+            output +=  this.id;
+            returnResult(output);
+        }
+    })
+}
